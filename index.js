6026f02d92eb4e3b26f894285768bf020c4befe9
Fiona Liausvia <fiona.liausvia@gmail.com>
2020-03-01 01:05:57 +0800

    Changed page style

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express 	= require('express')
+const path 		= require('path')
+const PORT 		= process.env.PORT || 5000
+const app       = express();
+const ejs 	   	= require("ejs");
+const bodyParser= require("body-parser");
+const http 		= require("http");
+const fs 		= require('fs');    
+const csv 		= require('csv-parser');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'data')))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
